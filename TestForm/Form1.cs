d52f0da9da6fb60b9aed6cb729f6630f1566169e
Ajmal Chavan <achavan@kognivera.com>
2026-04-10 19:42:49 +0530

    Initial commit

---
+++ TestForm/Form1.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TestForm.data;
+
+namespace TestForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string filePath = Path.Combine(Application.StartupPath, "data.json");
+
+                string json = File.ReadAllText(filePath);
+
+                Root data = JsonConvert.DeserializeObject<Root>(json);
+
+                txtOutput.Clear();
+
+                foreach (var student in data.students)
+                {
+                    txtOutput.AppendText(
+                        $"ID: {student.id}, Name: {student.name}, Age: {student.age}"
+                        + Environment.NewLine
+                    );
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ TestForm/data/student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestForm.data
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+    }
+
+    public class Root
+    {
+        public List<Student> students { get; set; }
+    }
+}
