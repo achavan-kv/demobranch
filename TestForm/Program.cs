3324bb587d673850accfce6f6ba5ae8fabf292e8
achavan-kv <achavan@kognivera.com>
2026-05-07 13:03:25 +0530

    Add division operation in Main method
    This change introduces a division operation that may lead to a divide by zero exception.

--- TestForm/Program.cs
+++ TestForm/Program.cs
@@ -14,6 +14,11 @@ namespace TestForm
         [STAThread]
         static void Main()
         {
+            int a,b,c;
+            a=0;
+            b=10;
+            c=b/a;
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
