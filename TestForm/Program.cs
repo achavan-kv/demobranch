8186cc3faa5226a88d3eda309819ae67f7b53293
achavan-kv <achavan@kognivera.com>
2026-05-27 00:14:42 +0530

    Add division of 200 by variable 'a'
    Added a new variable 'x' that divides 200 by 'a'.

--- TestForm/Program.cs
+++ TestForm/Program.cs
@@ -18,6 +18,7 @@ namespace TestForm
             a=0;
             b=10;
             c= b/a;
+            int x= 200/a;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
